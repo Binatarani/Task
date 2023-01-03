48a62becb11f6853b29d26619002fb96789e9d54
Binatarani <binatarani.bd@gmail.com>
2023-01-03 16:09:22 +0600

    Task 1

---
+++ script.js
@@ -0,0 +1,85 @@
+// Heading
+//1)
+const id1 = document.getElementById("div-text");
+const newHeading = document.createElement("h1");
+newHeading.innerText = "This is Heading one";
+id1.appendChild(newHeading);
+id1.style.color = "blue";
+id1.style.backgroundColor = "white";
+id1.style.textAlign = "center";
+id1.style.borderRadius = "10px";
+id1.style.padding = "1px";
+id1.style.fontSize = "15px";
+
+// 2)
+const id2 = document.getElementById("div-text2");
+const newHeading2 = document.createElement("h2");
+newHeading2.innerText = "This is Heading Two";
+id2.appendChild(newHeading2);
+id2.style.color = "red";
+id2.style.backgroundColor = "white";
+id2.style.textAlign = "center";
+id2.style.borderRadius = "10px";
+id2.style.marginTop = "10px";
+id2.style.padding = "1px";
+id2.style.fontSize = "15px";
+
+// 3
+
+const id3 = document.getElementById("div-text3");
+const newHeading3 = document.createElement("h3");
+newHeading3.innerText = "This is Heading Three";
+id3.appendChild(newHeading3);
+id3.style.color = "gray";
+id3.style.backgroundColor = "white";
+id3.style.textAlign = "center";
+id3.style.borderRadius = "10px";
+id3.style.marginTop = "10px";
+id3.style.padding = "1px";
+id3.style.fontSize = "15px";
+
+//4
+
+const id4 = document.getElementById("div-text4");
+const newHeading4 = document.createElement("h4");
+newHeading4.innerText = "This is Heading Four";
+id4.appendChild(newHeading4);
+id4.style.color = "green";
+id4.style.backgroundColor = "white";
+id4.style.textAlign = "center";
+id4.style.borderRadius = "10px";
+id4.style.marginTop = "10px";
+id4.style.padding = "1px";
+id4.style.fontSize = "15px";
+
+// 5
+
+const id5 = document.getElementById("div-text5");
+const newHeading5 = document.createElement("h5");
+newHeading5.innerText = "This is Heading Five";
+id5.appendChild(newHeading5);
+id5.style.color = "black";
+id5.style.backgroundColor = "white";
+id5.style.textAlign = "center";
+id5.style.borderRadius = "10px";
+id5.style.marginTop = "10px";
+id5.style.padding = "1px";
+id5.style.fontSize = "15px";
+
+// 6
+const id6 = document.getElementById("div-text6");
+const newHeading6 = document.createElement("h6");
+newHeading6.innerText = "This is Heading Six";
+id6.appendChild(newHeading6);
+id6.style.color = "orange";
+id6.style.backgroundColor = "white";
+id6.style.textAlign = "center";
+id6.style.borderRadius = "10px";
+id6.style.marginTop = "10px";
+id6.style.padding = "1px";
+id6.style.fontSize = "15px";
+
+const id7 = document.getElementById("div-text-one");
+const newHeading7 = document.createElement("p");
+newHeading7.innerText = "This is Paragraph!";
+id7.appendChild(newHeading7);
